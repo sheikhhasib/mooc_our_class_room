a43c5d19001bbf42f3202afa5f0fc54abb8e13da
sheikhhasib <hasib2130@gmail.com>
2021-11-15 17:46:30 +0600

    update online class

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $(document).on("click", ".course-title", function () {
+       let display =  $('.course-body').css('display');
+       if(display == 'none'){
+            $('.course-body').css('display','block');
+            $('#upArrow').css('display','block');
+            $('#downArrow').css('display','none');
+       }else{
+            $('.course-body').css('display','none');
+            $('#upArrow').css('display','none');
+            $('#downArrow').css('display','block');
+       }
+	});
+
+
+    // Navbar Scroll function
+    $(document).ready(function(){       
+        var scroll_pos = 0;
+        $(document).scroll(function() { 
+            scroll_pos = $(this).scrollTop();
+            if(scroll_pos > 200) {
+                $('#courseImage').css('display','none');
+                $('.main-content-right').css('top','30px');
+                $('.main-content-right').css('position','fixed');
+                $('.main-content-right').css('transition','0.5s');
+            } else {
+                $('#courseImage').css('display','block');
+                $('.main-content-right').css('top','100px');
+                $('.main-content-right').css('transition','0.5s');
+            }
+        });
+    });
+     // Navbar Scroll function Finish 
+});
